c67aeaee79f6c65b264d0531a6b41822c9f48edd
Kyle Hughes <kyle.hughes025@gmail.com>
2019-04-05 21:21:38 -0400

    Added google authentication

--- angular-frontend/src/app/dashboard/dashboard.component.ts
+++ angular-frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from "angularx-social-login";
+import {GoogleLoginProvider,SocialUser } from "angularx-social-login";
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private authService: AuthService) { }
+  user : SocialUser;
   ngOnInit() {
+    this.authService.authState.subscribe((user) => {
+      console.log("Received something from google ");
+      console.log(user);
+      this.user=user;
+    });
+  }
+
+  signInWithGoogle(): void {
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID);
+    console.log("after signin");
   }
 
 }
