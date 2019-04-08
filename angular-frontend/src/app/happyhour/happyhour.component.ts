bfc1606bae30e81f6648b3e64c347b5b9d6a516a
Kyle Hughes <kyle.hughes025@gmail.com>
2019-04-07 21:08:06 -0400

    Remove array of happy hours in component and just use the async pipe to element extra http request. Modify json to return specials based on the current date

--- angular-frontend/src/app/happyhour/happyhour.component.ts
+++ angular-frontend/src/app/happyhour/happyhour.component.ts
@@ -13,11 +13,11 @@ import {ConfirmDialogComponent} from '../shared/confirm-dialog/confirm-dialog.co
   styleUrls: ['./happyhour.component.scss']
 })
 export class HappyhourComponent implements OnInit {
-
+  today: number = Date.now();
   //our variable of our module to handle the dialog itself
   newHappyHourDialog: MatDialogRef<HappyHourCreateModalComponent>;
   //used for async pipe in html
-  happyHours: Observable<HappyHourModel[]>;
+  happyHours$: Observable<HappyHourModel[]>;
   //store them in a format this class can use 
   happyHoursArr : HappyHourModel[];
   constructor( private snackbar: MatSnackBar,
@@ -35,13 +35,9 @@ export class HappyhourComponent implements OnInit {
 
   refreshComponent () {
     //service returns the observable for the async pipe
-    //async pipe makes the http request return twice? weird occurrence
-    this.happyHours = this.happyhourServ.getHappyHours();
-    //save the array to use when edit is clicked, TODO this makes 2 calls to api :/ since
-    //we use async pipe - check article
-    this.happyHours.subscribe((dataArray : HappyHourModel[]) => { 
-      this.happyHoursArr = dataArray;
-    });
+    this.happyHours$ = this.happyhourServ.getHappyHours();
+    //originall had another subscribe here to store the arr locally (caused another http request) 
+    //but now just pass the full object to the edit and delete
   }
 
   toggleFormDialog() {
@@ -56,15 +52,12 @@ export class HappyhourComponent implements OnInit {
     });
     this.registerModalClose();
   }
-  editHappyHour(id: string) {
-
-    //already pulled all of the data so just filter on the id we want to edit
-      let happyHour : HappyHourModel = this.happyHoursArr.find((item) => item._id ===id);
+  editHappyHour(obj : HappyHourModel) {
       this.newHappyHourDialog = this.dialog.open(HappyHourCreateModalComponent, {
         hasBackdrop: false,
         closeOnNavigation: true,
         disableClose: false,
-        data: happyHour,
+        data: obj,
         width: '900px'
       });
       this.registerModalClose();
--- angular-frontend/src/app/happyhour/happyhour.service.ts
+++ angular-frontend/src/app/happyhour/happyhour.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Observable } from 'rxjs';
-import 'rxjs/add/operator/map';
+import {map} from 'rxjs/operators';
 import { environment } from '../../environments/environment'
 import { HappyHourModel } from '../models/HappyHourModel';
 
@@ -13,46 +13,60 @@ const APIURL = environment.apiUrl;
 export class HappyhourService {
 
   updateHappyHour(id: string, data: HappyHourModel): Observable<any> {
-    return this.http.put(APIURL + 'happyhour/'+id, data).map(response => {
+    return this.http.put(APIURL + 'happyhour/'+id, data).pipe(map(response => {
       console.log( response);
       return response;
-    });
+    }));
   }
 
   constructor(private http: HttpClient) { }
 
   //made async work (it needs to have an observable and it has to be an iterable like an array)
   getHappyHours(): Observable<HappyHourModel[]> {
-    return this.http.get(APIURL + 'happyhour').map(response => {
-      //some reason this loops over twice
+    return this.http.get(APIURL + 'happyhour').pipe(map(response => {
+      
+      const currentDay = this.getTodaysSpecials();
+      //do some error checking here eventually
       let resp: any = response || {};
-      console.log( resp.data);
-      return resp.data;
-    });
+      let jsonArray : HappyHourModel[] = resp.data;
+      //use the js map to dynamically modify todaysSpecials for each happy hour
+      jsonArray.map((single : HappyHourModel) => {single.todaysSpecials = single[currentDay]});
+      return jsonArray;
+    }));
 
   }
 
+  //Determine which specials to display for the current date
+  private getTodaysSpecials() : string{
+    const weekdays = new Array(
+      "sunSpecials","monSpecials", "tueSpecials", "wedSpecials", "thrSpecials", 
+        "friSpecials", "satSpecials"
+    );
+    const day = new Date().getDay();
+    return weekdays[day];
+  }
+
   getHappyHour(id: string): Observable<HappyHourModel[]> {
-    return this.http.get(APIURL + 'happyhour/' + id).map(response => {
+    return this.http.get(APIURL + 'happyhour/' + id).pipe(map(response => {
       let resp: any = response;
 
       //data should fit to the model
       return resp.data;
-    });
+    }));
 
   }
 
   postNewHappyHour(data): Observable<any> {
-    return this.http.post(APIURL + 'happyhour', data).map(response => {
+    return this.http.post(APIURL + 'happyhour', data).pipe(map(response => {
       return response;
-    });
+    }));
 
   }
 
   deleteHappyHour(id : string) : Observable<any> {
     console.log("DELETEING " + id);
-    return this.http.delete(APIURL + 'happyhour/' +id).map(response => {
+    return this.http.delete(APIURL + 'happyhour/' +id).pipe(map(response => {
       return response;
-    })
+    }));
   }
 }
