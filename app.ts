8c38079f958a3bdaed42b1c1d910ba2983f4c8ea
Kyle Hughes <kyle.hughes025@gmail.com>
2019-06-02 11:25:49 -0400

    Catch db connection errors from atlas

--- app.ts
+++ app.ts
@@ -4,54 +4,59 @@ import * as bodyParser from "body-parser";
 import * as cookieParser from "cookie-parser";
 import * as path from "path";
 import databaseConfig from './config/config';
-import {hhRoutes} from "./src/routes/index";
-import {userRoutes} from "./src/routes/userRoutes";
+import { hhRoutes } from "./src/routes/index";
+import { userRoutes } from "./src/routes/userRoutes";
 import * as mongoose from "mongoose";
 
 class App {
-    public app: express.Application;
-   
-    constructor() {
-        this.app = express();
-        this.config();
-        //this.routePrv.routes(this.app);
-        this.mongoSetup();
-        this.routes();
-    }
+  public app: express.Application;
 
-    private config(): void{
-        this.app.use(bodyParser.json());
-        this.app.use(bodyParser.urlencoded({ extended: false }));
-        // serving static files
-        this.app.use(express.static('public'));
-    }
-
-    private routes(): void {
-
-        this.app.use(cookieParser());
-        this.app.use('/api/happyhour/', hhRoutes);
-        this.app.use('/api/user/', userRoutes);
-        //catch all other bad api routes
-        this.app.use('/api/*',function(req, res, next) {
-          next('The api route was not found');
-        });
-        //let angular handle the routes
-        this.app.get('*', (req: express.Request, res: express.Response) => { 
-          res.sendFile(path.resolve('public/index.html')); 
-        });
-
-        // error handler, send back to client
-        this.app.use(function(err, req, res, next) {
-          res.status(404).send(err);
-        });
+  constructor() {
+    this.app = express();
+    this.config();
+    //this.routePrv.routes(this.app);
+    this.mongoSetup();
+    this.routes();
   }
-    //Using mongodb atlas
-    private mongoSetup(): void{
-        //public mongoUrl: string = 'mongodb://localhost:27017/PhillyFun';
-        const userPass = `${databaseConfig.user}:${databaseConfig.password}`;
-        const mongoUrl =`mongodb+srv://${userPass}@happyhourcluster-obkbn.mongodb.net/test?retryWrites=true`;
-        mongoose.connect(mongoUrl);
+
+  private config(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: false }));
+    // serving static files
+    this.app.use(express.static('public'));
+  }
+
+  private routes(): void {
+
+    this.app.use(cookieParser());
+    this.app.use('/api/happyhour/', hhRoutes);
+    this.app.use('/api/user/', userRoutes);
+    //catch all other bad api routes
+    this.app.use('/api/*', function (req, res, next) {
+      next('The api route was not found');
+    });
+    //let angular handle the routes
+    this.app.get('*', (req: express.Request, res: express.Response) => {
+      res.sendFile(path.resolve('public/index.html'));
+    });
+
+    // error handler, send back to client
+    this.app.use(function (err, req, res, next) {
+      res.status(404).send(err);
+    });
+  }
+  //Using mongodb atlas
+  private mongoSetup(): void {
+    //public mongoUrl: string = 'mongodb://localhost:27017/PhillyFun';
+    const userPass = `${databaseConfig.user}:${databaseConfig.password}`;
+    const mongoUrl = `mongodb+srv://${userPass}@happyhourcluster-obkbn.mongodb.net/test?retryWrites=true`;
+    mongoose.connect(mongoUrl).then(() => {
+      console.log("Connected to Database")
+    }, err => {
+      console.log("There was an error connecting to the mongo cluster", err);
     }
+    );
+  }
 
 }
 
